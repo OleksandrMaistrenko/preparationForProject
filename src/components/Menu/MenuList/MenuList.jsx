abd507ade58a3c9c94d5da9e81d2838ffc8f1f23
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-05-02 21:57:21 +0200

    useEffec backend

--- src/components/Menu/MenuList/MenuList.jsx
+++ src/components/Menu/MenuList/MenuList.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuItem from "../MenuItem/MenuItem";
-import pizzas from "../../../server/Data/Data";
+//import pizzas from "../../../server/Data/Data";
+import { useEffect } from "react";
 
 const MenuList = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    const getAllPizzas = async () => {
+      try {
+        const res = await fetch(
+          "https://react-fast-pizza-api.onrender.com/api/menu"
+        );
+        if (!res.ok) {
+          throw new Error("Failed to feth");
+        }
+        const data = await res.json();
+
+        setMenu(data.data);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getAllPizzas();
+  }, []);
+
   return (
     <ul>
-      {pizzas.map((pizza) => (
+      {menu.map((pizza) => (
         <MenuItem key={pizza.id} item={pizza} />
       ))}
     </ul>
