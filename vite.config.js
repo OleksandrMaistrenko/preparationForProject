fa01af8fa122d8b7229bf7f6af0691bed27d6572
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-05-25 10:19:28 +0200

    about

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://oleksandrmaistrenko.github.io/",
+  base: "https://oleksandrmaistrenko.github.io/about",
 });
